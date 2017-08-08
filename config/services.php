8c3fc6eacd1f7146f149fcab74a10f54c0913cb1
JP Caparas <jp@jpcaparas.com>
2017-08-08 22:36:06 +1200

    Add NEXMO_SMS_FROM config key

--- config/services.php
+++ config/services.php
@@ -38,6 +38,6 @@ return [
     'nexmo' => [
         'key' => env('NEXMO_KEY'),
         'secret' => env('NEXMO_SECRET'),
-        'sms_from' => '15556666666',
+        'sms_from' => env('NEXMO_SMS_FROM', 15556666666),
     ],
 ];
