7daf20769269c420746ff4c743eced4850ed4783
JP Caparas <jp@jpcaparas.com>
2017-08-10 10:06:27 +1200

    Fix wrong route name

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 Route::group(['prefix' => 'notifications', 'namespace' => 'Notifications'], function() {
     Route::get('nexmo/{user}', 'NexmoController')->name('notifications.nexmo');
     Route::get('db/{user}', 'DbController')->name('notifications.db');
-    Route::get('slack/{user}', 'SlackController')->name('slack.db');
+    Route::get('slack/{user}', 'SlackController')->name('notifications.slack');
 });
